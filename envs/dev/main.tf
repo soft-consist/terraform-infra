4f33d2f426cedb82019bbf9ad14ab2376c853651
ashubambal <ashutoshbambal97@gmail.com>
2026-01-18 22:05:15 +0530

    dev104

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -2,16 +2,16 @@ provider "aws" {
   region = var.region
 }
 
-module "vpc" {
-  source          = "git::https://github.com/soft-consist/terraform-modules.git//modules/vpc?ref=main"
-  env             = var.env
-  cidr_block      = var.cidr_block
-  tags            = var.tags
-  region          = var.region
-  public_subnets  = var.public_subnets
-  private_subnets = var.private_subnets
-  azs             = var.azs
-}
+# module "vpc" {
+#   source          = "git::https://github.com/soft-consist/terraform-modules.git//modules/vpc?ref=main"
+#   env             = var.env
+#   cidr_block      = var.cidr_block
+#   tags            = var.tags
+#   region          = var.region
+#   public_subnets  = var.public_subnets
+#   private_subnets = var.private_subnets
+#   azs             = var.azs
+# }
 
 # module "eks" {
 #   source              = "git::https://github.com/soft-consist/terraform-modules.git//modules/eks?ref=main"
