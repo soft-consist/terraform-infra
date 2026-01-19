e0513c6768a1c656cfb99ebb3ade58f3d5db09bf
ashubambal <ashutoshbambal97@gmail.com>
2026-01-19 17:09:04 +0530

    dev114

--- bootstrap/github-actions/main.tf
+++ bootstrap/github-actions/main.tf
@@ -2,14 +2,10 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-data "aws_caller_identity" "current" {}
-
 resource "aws_iam_openid_connect_provider" "github" {
   url = "https://token.actions.githubusercontent.com"
 
-  client_id_list = [
-    "sts.amazonaws.com"
-  ]
+  client_id_list = ["sts.amazonaws.com"]
 
   thumbprint_list = [
     "6938fd4d98bab03faadb97b34396831e3780aea1"
@@ -22,10 +18,12 @@ resource "aws_iam_role" "github_actions" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
+
+      # 1️⃣ GitHub Actions (OIDC)
       {
         Effect = "Allow"
         Principal = {
-          Federated = data.aws_iam_openid_connect_provider.github.arn
+          Federated = aws_iam_openid_connect_provider.github.arn
         }
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
@@ -34,13 +32,14 @@ resource "aws_iam_role" "github_actions" {
           }
         }
       },
+
+      # 2️⃣ Human access (local laptop users)
       {
         Effect = "Allow"
         Principal = {
           AWS = [
             "arn:aws:iam::358871393576:user/Kalyani-Bambal",
-            "arn:aws:iam::358871393576:user/Ashutosh-Bambal",
-            "arn:aws:iam::358871393576:role/github-actions-terraform-role"
+            "arn:aws:iam::358871393576:user/Ashutosh-Bambal"
           ]
         }
         Action = "sts:AssumeRole"
