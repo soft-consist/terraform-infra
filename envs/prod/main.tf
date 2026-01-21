8628df55aabcef2e6979d5b413186879a9677d3b
ashubambal <ashutoshbambal97@gmail.com>
2026-01-21 12:33:10 +0530

    tkt3

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -2,6 +2,22 @@ provider "aws" {
   region = var.region
 }
 
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_ca)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      module.eks.cluster_name
+    ]
+  }
+}
+
 module "vpc" {
   source          = "git::https://github.com/soft-consist/terraform-modules.git//modules/vpc?ref=v9.0.7"
   env             = var.env
