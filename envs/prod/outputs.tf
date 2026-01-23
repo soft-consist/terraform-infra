d810932993b7d0132b4f12456f1e68e29c0bf9ff
Ashutosh Bambal <92073828+ashubambal@users.noreply.github.com>
2026-01-23 22:32:50 +0530

    dest (#41)

--- envs/prod/outputs.tf
+++ envs/prod/outputs.tf
@@ -1,34 +1,34 @@
-output "aws_vpc" {
-  value = module.vpc.aws_vpc
-}
+# output "aws_vpc" {
+#   value = module.vpc.aws_vpc
+# }
 
-output "region" {
-  value = var.region
-}
+# output "region" {
+#   value = var.region
+# }
 
-output "private_subnet_ids" {
-  value = module.vpc.private_subnet_ids
-}
+# output "private_subnet_ids" {
+#   value = module.vpc.private_subnet_ids
+# }
 
-output "cluster_name" {
-  description = "EKS Cluster Name"
-  value       = module.eks.cluster_name
-}
+# output "cluster_name" {
+#   description = "EKS Cluster Name"
+#   value       = module.eks.cluster_name
+# }
 
-output "cluster_endpoint" {
-  description = "EKS Cluster Endpoint"
-  value       = module.eks.cluster_endpoint
-}
+# output "cluster_endpoint" {
+#   description = "EKS Cluster Endpoint"
+#   value       = module.eks.cluster_endpoint
+# }
 
-output "cluster_ca" {
-  description = "EKS Cluster CA"
-  value       = module.eks.cluster_ca
-}
+# output "cluster_ca" {
+#   description = "EKS Cluster CA"
+#   value       = module.eks.cluster_ca
+# }
 
-output "public_subnet_ids" {
-  value = module.vpc.public_subnet_ids
-}
+# output "public_subnet_ids" {
+#   value = module.vpc.public_subnet_ids
+# }
 
-output "bastion_access_role_arn" {
-  value = module.bastion.bastion_access_role_arn
-}
+# output "bastion_access_role_arn" {
+#   value = module.bastion.bastion_access_role_arn
+# }
