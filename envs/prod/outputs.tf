bce4701788033d370a32eb1314491d79ac8ba47a
ashubambal <ashutoshbambal97@gmail.com>
2026-01-26 08:40:49 +0530

    up300

--- envs/prod/outputs.tf
+++ envs/prod/outputs.tf
@@ -1,42 +1,42 @@
-#  output "aws_vpc" {
-#    value = module.vpc.aws_vpc
-#  }
+ output "aws_vpc" {
+   value = module.vpc.aws_vpc
+ }
 
-#  output "region" {
-#    value = var.region
-#  }
+ output "region" {
+   value = var.region
+ }
 
-#  output "private_subnet_ids" {
-#    value = module.vpc.private_subnet_ids
-#  }
+ output "private_subnet_ids" {
+   value = module.vpc.private_subnet_ids
+ }
 
-#  output "cluster_name" {
-#    description = "EKS Cluster Name"
-#    value       = module.eks.cluster_name
-#  }
+ output "cluster_name" {
+   description = "EKS Cluster Name"
+   value       = module.eks.cluster_name
+ }
 
-#  output "cluster_endpoint" {
-#    description = "EKS Cluster Endpoint"
-#    value       = module.eks.cluster_endpoint
-#  }
+ output "cluster_endpoint" {
+   description = "EKS Cluster Endpoint"
+   value       = module.eks.cluster_endpoint
+ }
 
-#  output "oidc_provider_url" {
-#    value = module.eks.oidc_provider_url
-#  }
+ output "oidc_provider_url" {
+   value = module.eks.oidc_provider_url
+ }
 
-#  output "oidc_provider_arn" {
-#    value = module.eks.oidc_provider_arn
-#  }
+ output "oidc_provider_arn" {
+   value = module.eks.oidc_provider_arn
+ }
 
-#  output "cluster_ca" {
-#    description = "EKS Cluster CA"
-#    value       = module.eks.cluster_ca
-#  }
+ output "cluster_ca" {
+   description = "EKS Cluster CA"
+   value       = module.eks.cluster_ca
+ }
 
-#  output "public_subnet_ids" {
-#    value = module.vpc.public_subnet_ids
-#  }
+ output "public_subnet_ids" {
+   value = module.vpc.public_subnet_ids
+ }
 
-#  output "bastion_access_role_arn" {
-#    value = module.bastion.bastion_access_role_arn
-#  }
+ output "bastion_access_role_arn" {
+   value = module.bastion.bastion_access_role_arn
+ }
