61d519a94a7921e3589e71f5558f4a0020b1f401
ashubambal <ashutoshbambal97@gmail.com>
2026-01-26 14:33:37 +0530

    adding argo

--- envs/prod/providers.tf
+++ envs/prod/providers.tf
@@ -19,3 +19,10 @@ provider "helm" {
     token                  = data.aws_eks_cluster_auth.this.token
   }
 }
+
+provider "kubectl" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+  load_config_file       = false
+}
