0b763cf5e599129bfa211439fbed3ecbd9c8ed25
Ashutosh Bambal <92073828+ashubambal@users.noreply.github.com>
2026-01-26 15:12:24 +0530

    adding argo (#68)
    * adding argo
    
    * adding argo
    
    * adding argo
    
    * adding argo
    
    * adding argo
    
    * adding argo
    
    * adding argo
    
    * adding argo
    
    * adding argo

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -1,39 +1,39 @@
 module "vpc" {
-   source          = "git::https://github.com/soft-consist/terraform-modules.git//modules/vpc?ref=v9.0.27"
-   env             = var.env
-   cidr_block      = var.cidr_block
-   tags            = var.tags
-   region          = var.region
-   public_subnets  = var.public_subnets
-   private_subnets = var.private_subnets
-   azs             = var.azs
- }
+  source          = "git::https://github.com/soft-consist/terraform-modules.git//modules/vpc?ref=v9.0.27"
+  env             = var.env
+  cidr_block      = var.cidr_block
+  tags            = var.tags
+  region          = var.region
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+  azs             = var.azs
+}
 
- module "eks" {
-   source              = "git::https://github.com/soft-consist/terraform-modules.git//modules/eks?ref=v9.0.27"
-   env                 = var.env
-   cluster_name        = var.cluster_name
-   cluster_version     = var.cluster_version
-   vpc_id              = module.vpc.aws_vpc
-   private_subnet_ids  = module.vpc.private_subnet_ids
-   tags                = var.tags
-   desired_size        = var.desired_size
-   max_size            = var.max_size
-   min_size            = var.min_size
-   node_instance_types = var.node_instance_types
-   allowd_cidr_blocks  = var.allowd_cidr_blocks
-   bastion_access_role_arn = module.bastion.bastion_access_role_arn
- }
+module "eks" {
+  source                  = "git::https://github.com/soft-consist/terraform-modules.git//modules/eks?ref=v9.0.27"
+  env                     = var.env
+  cluster_name            = var.cluster_name
+  cluster_version         = var.cluster_version
+  vpc_id                  = module.vpc.aws_vpc
+  private_subnet_ids      = module.vpc.private_subnet_ids
+  tags                    = var.tags
+  desired_size            = var.desired_size
+  max_size                = var.max_size
+  min_size                = var.min_size
+  node_instance_types     = var.node_instance_types
+  allowd_cidr_blocks      = var.allowd_cidr_blocks
+  bastion_access_role_arn = module.bastion.bastion_access_role_arn
+}
 
- module "bastion" {
-   source = "git::https://github.com/soft-consist/terraform-modules.git//modules/bastion?ref=v9.0.27"
-   env    = var.env
-   tags             = var.tags
-   bastion_assume_role_principals = var.bastion_assume_role_principals
- }
+module "bastion" {
+  source                         = "git::https://github.com/soft-consist/terraform-modules.git//modules/bastion?ref=v9.0.27"
+  env                            = var.env
+  tags                           = var.tags
+  bastion_assume_role_principals = var.bastion_assume_role_principals
+}
 
 module "addons" {
-  source = "git::https://github.com/soft-consist/terraform-modules.git//modules/addons?ref=v9.0.27"
+  source             = "git::https://github.com/soft-consist/terraform-modules.git//modules/addons?ref=v9.0.27"
   cluster_name       = module.eks.cluster_name
   cni_version        = var.cni_version
   coredns_version    = var.coredns_version
@@ -44,7 +44,7 @@ module "addons" {
 }
 
 module "argocd" {
-  source = "git::https://github.com/soft-consist/terraform-modules.git//modules/argocd?ref=v9.0.31"
+  source       = "git::https://github.com/soft-consist/terraform-modules.git//modules/argocd?ref=v9.0.34"
   cluster_name = module.eks.cluster_name
   values = [
     file("${path.module}/argocd-values.yaml")
