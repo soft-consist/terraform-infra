a98085d97656ef2f27af65732d7cf12e0d66a69d
ashubambal <ashutoshbambal97@gmail.com>
2026-02-07 22:18:18 +0530

    auth

--- envs/prod/aws-auth.tf
+++ envs/prod/aws-auth.tf
@@ -1,26 +1,26 @@
-resource "kubernetes_config_map" "aws_auth" {
-  metadata {
-    name      = "aws-auth"
-    namespace = "kube-system"
-  }
+# resource "kubernetes_config_map" "aws_auth" {
+#   metadata {
+#     name      = "aws-auth"
+#     namespace = "kube-system"
+#   }
 
-  data = {
-    mapRoles = yamlencode([
-      {
-        rolearn  = "arn:aws:iam::358871393576:role/prod-eks-node-role"
-        username = "system:node:{{EC2PrivateDNSName}}"
-        groups = [
-          "system:bootstrappers",
-          "system:nodes"
-        ]
-      },
-      {
-        rolearn  = "arn:aws:iam::358871393576:role/github-actions-terraform-role"
-        username = "github-actions"
-        groups = [
-          "system:masters"
-        ]
-      }
-    ])
-  }
-}
+#   data = {
+#     mapRoles = yamlencode([
+#       {
+#         rolearn  = "arn:aws:iam::358871393576:role/prod-eks-node-role"
+#         username = "system:node:{{EC2PrivateDNSName}}"
+#         groups = [
+#           "system:bootstrappers",
+#           "system:nodes"
+#         ]
+#       },
+#       {
+#         rolearn  = "arn:aws:iam::358871393576:role/github-actions-terraform-role"
+#         username = "github-actions"
+#         groups = [
+#           "system:masters"
+#         ]
+#       }
+#     ])
+#   }
+# }
