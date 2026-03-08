386e2ae2d553e5c85bc5688b0981316b254885c8
ashubambal <ashutoshbambal97@gmail.com>
2026-03-08 07:22:45 +0530

    changes

--- envs/prod/backend.tf
+++ envs/prod/backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-ashu-abc"
-    key            = "prod-terraform.tfstate"
-    region         = "ap-northeast-2"
-    dynamodb_table = "prod-terraform-locks"
-    encrypt        = true
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "my-terraform-state-ashu-abc"
+#     key            = "prod-terraform.tfstate"
+#     region         = "ap-northeast-2"
+#     dynamodb_table = "prod-terraform-locks"
+#     encrypt        = true
+#   }
+# }
