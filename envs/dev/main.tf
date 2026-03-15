bb9c435c8caaa2d34bebb6bc392251a13300f053
softconsist-in <softconsist@gmail.com>
2026-03-16 00:07:04 +0530

    down (#110)
    Co-authored-by: ashubambal <ashutoshbambal97@gmail.com>

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,39 +1,39 @@
-module "vpc" {
-  source          = "git::https://github.com/soft-consist/terraform-modules.git//modules/vpc?ref=v9.0.35"
-  env             = var.env
-  region          = var.region
-  cidr_block      = var.cidr_block
-  azs             = var.azs
-  public_subnets  = var.public_subnets
-  private_subnets = var.private_subnets
-  tags            = var.tags
-}
+# module "vpc" {
+#   source          = "git::https://github.com/soft-consist/terraform-modules.git//modules/vpc?ref=v9.0.35"
+#   env             = var.env
+#   region          = var.region
+#   cidr_block      = var.cidr_block
+#   azs             = var.azs
+#   public_subnets  = var.public_subnets
+#   private_subnets = var.private_subnets
+#   tags            = var.tags
+# }
 
-module "eks" {
-  source              = "git::https://github.com/soft-consist/terraform-modules.git//modules/eks?ref=v9.0.35"
-  env                 = var.env
-  vpc_id              = module.vpc.aws_vpc
-  cluster_name        = var.cluster_name
-  cluster_version     = var.cluster_version
-  private_subnet_ids  = module.vpc.private_subnet_ids
-  desired_size        = var.desired_size
-  max_size            = var.max_size
-  min_size            = var.min_size
-  node_instance_types = var.node_instance_types
-  allowd_cidr_blocks  = var.allowd_cidr_blocks
-  tags                = var.tags
-}
+# module "eks" {
+#   source              = "git::https://github.com/soft-consist/terraform-modules.git//modules/eks?ref=v9.0.35"
+#   env                 = var.env
+#   vpc_id              = module.vpc.aws_vpc
+#   cluster_name        = var.cluster_name
+#   cluster_version     = var.cluster_version
+#   private_subnet_ids  = module.vpc.private_subnet_ids
+#   desired_size        = var.desired_size
+#   max_size            = var.max_size
+#   min_size            = var.min_size
+#   node_instance_types = var.node_instance_types
+#   allowd_cidr_blocks  = var.allowd_cidr_blocks
+#   tags                = var.tags
+# }
 
-module "addons" {
-  source             = "git::https://github.com/soft-consist/terraform-modules.git//modules/addons?ref=v9.0.35"
-  cluster_name       = module.eks.cluster_name
-  cni_version        = var.cni_version
-  coredns_version    = var.coredns_version
-  kube_proxy_version = var.kube_proxy_version
-}
+# module "addons" {
+#   source             = "git::https://github.com/soft-consist/terraform-modules.git//modules/addons?ref=v9.0.35"
+#   cluster_name       = module.eks.cluster_name
+#   cni_version        = var.cni_version
+#   coredns_version    = var.coredns_version
+#   kube_proxy_version = var.kube_proxy_version
+# }
 
-module "eks-access" {
-  source             = "git::https://github.com/soft-consist/terraform-modules.git//modules/eks-access?ref=v9.0.36"
-  cluster_name = var.cluster_name
-  access_entries = var.access_entries
-}
+# module "eks-access" {
+#   source             = "git::https://github.com/soft-consist/terraform-modules.git//modules/eks-access?ref=v9.0.36"
+#   cluster_name = var.cluster_name
+#   access_entries = var.access_entries
+# }
--- envs/dev/outputs.tf
+++ envs/dev/outputs.tf
@@ -1,20 +1,20 @@
-output "aws_vpc" {
-  value = module.vpc.aws_vpc
-}
+# output "aws_vpc" {
+#   value = module.vpc.aws_vpc
+# }
 
-output "region" {
-  value = var.region
-}
+# output "region" {
+#   value = var.region
+# }
 
-output "private_subnet_ids" {
-  value = module.vpc.private_subnet_ids
-}
+# output "private_subnet_ids" {
+#   value = module.vpc.private_subnet_ids
+# }
 
-output "cluster_name" {
-  description = "EKS Cluster Name"
-  value       = module.eks.cluster_name
-}
+# output "cluster_name" {
+#   description = "EKS Cluster Name"
+#   value       = module.eks.cluster_name
+# }
 
-output "access_entries" {
-  value = module.eks-access.access_entries
-}
+# output "access_entries" {
+#   value = module.eks-access.access_entries
+# }
