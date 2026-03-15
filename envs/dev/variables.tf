64ebeed741f95b4cb53b432da5ae0864ca93da56
softconsist-in <softconsist@gmail.com>
2026-03-15 08:55:25 +0530

    a5 (#106)
    * a5
    
    * a5
    
    * a5
    
    ---------
    
    Co-authored-by: ashubambal <ashutoshbambal97@gmail.com>

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -32,6 +32,8 @@ variable "private_subnets" {
   type        = list(string)
 }
 
+# EKS variables
+
 variable "cluster_name" {
   description = "EKS Cluster Name"
   type        = string
@@ -40,18 +42,6 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "EKS Cluster Version"
   type        = string
-}
-
-variable "vpc_id" {
-  description = "VPC ID"
-  type        = string
-  default     = ""
-}
-
-variable "private_subnet_ids" {
-  description = "Private Subnets IDs"
-  type        = list(string)
-  default     = []
 }
 
 variable "desired_size" {
