896d63710f7cc938cbbcb1eb3cda8dffcf090499
ashubambal <ashutoshbambal97@gmail.com>
2026-03-16 03:41:28 +0000

    fixargocd

--- envs/dev/providers.tf
+++ envs/dev/providers.tf
@@ -1,3 +1,26 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+# First Commnent out below 
+
+data "aws_eks_cluster" "eks" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = var.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
+provider "kubectl" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+  load_config_file       = false
+}
