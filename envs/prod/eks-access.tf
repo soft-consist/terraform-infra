ee8fe8379ea5e96cd8206bd3a57966b22b879a25
ashubambal <ashutoshbambal97@gmail.com>
2026-03-16 03:14:32 +0000

    argocd

--- envs/prod/eks-access.tf
+++ envs/prod/eks-access.tf
@@ -1,8 +1,8 @@
-resource "aws_eks_access_entry" "github_actions" {
-  cluster_name  = module.eks.cluster_name
-  principal_arn = "arn:aws:iam::358871393576:role/github-actions-terraform-role"
-  type          = "STANDARD"
-}
+# resource "aws_eks_access_entry" "github_actions" {
+#   cluster_name  = module.eks.cluster_name
+#   principal_arn = "arn:aws:iam::358871393576:role/github-actions-terraform-role"
+#   type          = "STANDARD"
+# }
 
 # resource "aws_eks_access_policy_association" "github_actions_admin" {
 #   cluster_name  = module.eks.cluster_name
