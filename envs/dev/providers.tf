e76708672135c70cb245fd77001ce5b64885f72a
ashubambal <ashutoshbambal97@gmail.com>
2026-03-16 04:07:18 +0000

    fixargocd

--- envs/dev/providers.tf
+++ envs/dev/providers.tf
@@ -46,6 +46,26 @@ provider "kubernetes" {
   }
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+
+      command = "aws"
+
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name",
+        module.eks.cluster_name
+      ]
+    }
+  }
+}
+
 provider "kubectl" {
   host                   = data.aws_eks_cluster.eks.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
