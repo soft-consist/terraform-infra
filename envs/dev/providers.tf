d3db55e3bbd46771e44f240a5e72311b87e1b97b
softconsist-in <softconsist@gmail.com>
2026-03-16 09:41:30 +0530

    fixargocd (#117)
    * fixargocd
    
    * fixargocd
    
    * fixargocd
    
    ---------
    
    Co-authored-by: ashubambal <ashutoshbambal97@gmail.com>

--- envs/dev/providers.tf
+++ envs/dev/providers.tf
@@ -28,9 +28,43 @@ data "aws_eks_cluster_auth" "eks" {
 }
 
 provider "kubernetes" {
+
   host                   = data.aws_eks_cluster.eks.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+
+    command = "aws"
+
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      var.cluster_name
+    ]
+  }
+}
+
+provider "helm" {
+
+  kubernetes {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+
+      command = "aws"
+
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name",
+        var.cluster_name
+      ]
+    }
+  }
 }
 
 provider "kubectl" {
