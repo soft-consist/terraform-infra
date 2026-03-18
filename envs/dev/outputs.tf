c3693a5b275dd7d395f005abb32f713eaaf9d7a2
ashubambal <ashutoshbambal97@gmail.com>
2026-03-18 03:15:45 +0000

    sysup

--- envs/dev/outputs.tf
+++ envs/dev/outputs.tf
@@ -1,22 +1,22 @@
-# output "aws_vpc" {
-#   value = module.vpc.aws_vpc
-# }
+output "aws_vpc" {
+  value = module.vpc.aws_vpc
+}
 
 output "region" {
   value = var.region
 }
 
-# output "private_subnet_ids" {
-#   value = module.vpc.private_subnet_ids
+output "private_subnet_ids" {
+  value = module.vpc.private_subnet_ids
+}
+
+output "cluster_name" {
+  description = "EKS Cluster Name"
+  value       = module.eks.cluster_name
+}
+
+# # aceess entries module
+
+# output "access_entries" {
+#   value = module.eks-access.access_entries
 # }
-
-# output "cluster_name" {
-#   description = "EKS Cluster Name"
-#   value       = module.eks.cluster_name
-# }
-
-# # # aceess entries module
-
-# # output "access_entries" {
-# #   value = module.eks-access.access_entries
-# # }
